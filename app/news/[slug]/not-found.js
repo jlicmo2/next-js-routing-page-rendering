417c0aea2c0b8298a9b8c4e5cfe85c3bef2c3be2
Jake Licmo <jlicmo2@academic.rrc.ca>
2024-05-10 10:28:08 -0500

    Parallel Routing

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,6 @@
+export default function NewsNotFound(){
+    return <div id="error">
+        <h1>Not Found!</h1>
+        <p>The requested news article could not be found.</p>
+    </div>
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import MainHeader from "./components/main-header/page";
+
 export default function HomePage() {
   return (
+    <>
     <div id="home">
-      <h1>Next.js Routing & Page Rendering</h1>
+      <h1>Next.js Rousting & Page Rendering</h1>
     </div>
+    </>
   );
 }
